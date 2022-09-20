9841bcb9290116d0319407bf2ad85d8861b66eef
Ashton MacKenzie <pikeminnow@gmail.com>
2022-09-20 17:44:46 -0400

    lab activities

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -6,7 +6,24 @@ function ProjectList({ projects }) {
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">
+        {/* render ProjectItem components here */}
+        {projects.map((project) => {
+          return <ProjectItem
+            key={project.id}
+            about={project.about}
+            name={project.name}
+            technologies={project.technologies} />;
+        })}
+
+        {/* <ProjectItem
+          key={projects.id}
+          about={projects.about}
+          name={projects.name}
+          technologies={projects.technologies}
+
+        /> */}
+      </div>
     </div>
   );
 }

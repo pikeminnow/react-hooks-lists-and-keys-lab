2d0ac1ea2f0e856acaea67d07d5b2f0d10233bde
Ashton MacKenzie <pikeminnow@gmail.com>
2022-09-20 11:46:32 -0400

    i still think this solution is cooler, fight me

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,18 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  const navLinks = links.map((link) => {
+    let linkEntries = Object.entries({ link });
+    console.log(linkEntries)
+    let crunchLink = "#" + linkEntries[0][1];
+    return <a href={crunchLink} >{link}</a>
+  })
+
+  return <nav>
+    {/* display an <a> tag for each link here */}
+    {navLinks}
+
+  </nav>;
 }
 
 export default NavBar;

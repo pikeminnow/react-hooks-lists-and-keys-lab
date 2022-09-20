6a0015241b9592647b28649c2c8d4e7678cf2aed
Ashton MacKenzie <pikeminnow@gmail.com>
2022-09-20 11:48:30 -0400

    using saner string interpolation

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,10 +4,7 @@ function NavBar() {
   const links = ["home", "about", "projects"];
 
   const navLinks = links.map((link) => {
-    let linkEntries = Object.entries({ link });
-    console.log(linkEntries)
-    let crunchLink = "#" + linkEntries[0][1];
-    return <a href={crunchLink} >{link}</a>
+    return <a href={'#' + link} >{link}</a>
   })
 
   return <nav>
